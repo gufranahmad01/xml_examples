874df8c8065d0f713870a913557e8a91904494d2
gufranahmad01 <er.gufranahmad422@gmail.com>
2022-12-02 23:07:02 +0530

    backup

---
+++ src/test/java/com/searchlead/Searchlead3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.searchlead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Searchlead3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
